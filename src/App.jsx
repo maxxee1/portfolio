2f07c70356375737f6010400af68344cfe7df0d8
Maximiliano <130422048+maxxee1@users.noreply.github.com>
2026-02-09 13:13:25 -0500

    Change image bucket name in App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const Portfolio = () => {
   const [activeSection, setActiveSection] = useState('home');
 
   // -------------------- CONST FOTOS -------------------
-const BUCKET = "img";
+const BUCKET = "rental-apartments-images";
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 
   const translations = {
