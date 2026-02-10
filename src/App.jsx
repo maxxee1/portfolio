48252173f260087e6ba8e9ab69fddeda00e84964
Maximiliano <130422048+maxxee1@users.noreply.github.com>
2026-02-10 13:44:49 -0500

    chiuta

--- src/App.jsx
+++ src/App.jsx
@@ -246,7 +246,7 @@ const Portfolio = () => {
     },
     {
       id: 6,
-      icon: '💬',
+      image: "images/proyects/npc.png",
       title: {
         es: 'Chat con Named Pipes',
         en: 'Named Pipes Chat'
